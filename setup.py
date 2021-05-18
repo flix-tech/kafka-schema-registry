79fa800818f1cd58a9fd9692672db9018b104053
Jacopo Farina <jacopo.farina@flixbus.com>
2021-05-18 17:09:46 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='kafka_schema_registry',
+
+    version='0.0.1',
+    description='Kafka and schema registry integration',
+    long_description=open('./README.md').read(),
+    author='Flixbus',
+    url='https://github.com/flix-tech/kafka-schema-registry',
+
+    python_requires='>=3.7',
+
+    packages=find_packages(),
+
+    install_requires=[
+        'fastavro',
+        'kafka-python',
+        'requests',
+    ],
+)
