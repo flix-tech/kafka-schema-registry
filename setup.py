e2ebc33b0458984f5df329e44751ff4f081fb732
Bastian Venthur <bastian.venthur@flixbus.com>
2021-07-05 08:56:51 +0200

    several fixes on setup.py:
    * use correct author/email
    * read version from file
    * read long description from readme
    * provide package name directly

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
 from setuptools import setup, find_packages
 
+meta = {}
+exec(open('./kafka_schema_registry/version.py').read(), meta)
+meta['long_description'] = open('./README.md').read()
+
 
 setup(
     name='kafka_schema_registry',
-
-    version='0.0.1',
+    version=meta['__VERSION__'],
     description='Kafka and schema registry integration',
-    long_description=open('./README.md').read(),
-    author='Flixbus',
+    long_description=meta['long_description'],
+    long_description_content_type='text/markdown',
+    keywords='kafka schema-registry',
+    author='FlixTech',
+    author_email="open-source@flixbus.com",
     url='https://github.com/flix-tech/kafka-schema-registry',
-
+    project_urls={
+        "Changelog": "https://github.com/flix-tech/kafka-schema-registry/blob/master/CHANGELOG.md",  # noqa
+        "Source": 'https://github.com/flix-tech/kafka-schema-registry',
+    },
     python_requires='>=3.7',
-
-    packages=find_packages(),
-
     install_requires=[
         'fastavro',
         'kafka-python',
         'requests',
     ],
+    packages=['kafka_schema_registry'],
     license='MIT',
 )
